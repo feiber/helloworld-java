1f77df457781b5e01ece6d795ce35ae9e7e8bf1c
Nathan <nathan.perito@gmail.com>
2011-09-19 20:56:27 -0300

    oi
    Signed-off-by: Nathan <nathan.perito@gmail.com>

---
+++ helloworld-java/src/Calculadora.java
@@ -0,0 +1,19 @@
+
+public class Calculadora {
+
+	public static int somar(int i, int j) {
+		// TODO Auto-generated method stub
+		return i + j;
+	}
+
+	public static int multiplicar(int i, int j) {
+		// TODO Auto-generated method stub
+		return i * j;
+	}
+
+	public static int dividir(int i, int j) {
+		// TODO Auto-generated method stub
+		return i / j;
+	}
+
+}
